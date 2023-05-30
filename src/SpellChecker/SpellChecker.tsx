7ecdd658746593e9eff3f28780b5445901ddd7de
likagogishvili <likagogishvili2@gmail.com>
2023-05-30 23:07:26 +0400

    added landing components

--- src/SpellChecker/SpellChecker.tsx
+++ src/SpellChecker/SpellChecker.tsx
@@ -1,8 +1,48 @@
-function SpellCkecker(){
+import "./spellCkecker.css";
+import * as pageimges from "../assets/PageIcons/PageIcons";
+function SpellCkecker() {
+    
+  return (
+    <div className="spelchecker">
+      <div className="spelchecker_top">
+        <button
+          type="button"
+          style={{ backgroundColor: "#2D62ED" }}
+          className="btn btn-primary btn_fonts"
+        >
+          <img src={pageimges.check} alt="" />
+          Check
+        </button>
 
-    return(
-        <p>SpellCkecker</p>
-    )
+        <div className="d-flex">
+          <button
+            type="button"
+            className="btn btn-light p-2 btn_fonts"
+            style={{ backgroundColor: "#EDEDED", color: "#2D62ED" }}
+          >
+            <img src={pageimges.copy} alt="" style={{ marginRight: "10px" }} />
+            Copy
+          </button>
 
+          <button
+            type="button"
+            className="btn btn-light p-2 btn_fonts mx-3"
+            style={{ backgroundColor: "#ffff", color: "#606060" }}
+          >
+            <img
+              src={pageimges.delete_bin}
+              alt=""
+              style={{ marginRight: "10px" }}
+            />
+            Delete
+          </button>
+          <div className="vl"></div>
+
+
+        </div>
+
+      </div>
+    </div>
+  );
 }
-export default SpellCkecker
+export default SpellCkecker;
---
+++ src/assets/PageIcons/PageIcons.tsx
@@ -0,0 +1,6 @@
+import check from "./check-line.svg";
+import delete_bin from "./delete-bin-line.svg";
+import copy from "./file-copy-line.svg";
+import eng from "./eng.svg";
+
+export { check, delete_bin, copy, eng };
